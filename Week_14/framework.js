5b3419f0e08861de4042c7dd995c143a869f6e11
alphabetX <alphabet_pc@inner.com>
2021-07-03 23:20:50 +0800

    feat(wk14):代码

--- Week_14/framework.js
+++ Week_14/framework.js
@@ -53,4 +53,69 @@ class TextWrapper extends Component {
     }
 }
 
-export { createElement, Component };
+const noop = () => {};
+class Drag {
+    constructor(target) {
+        this.dragstart = this.dragstart.bind(this);
+        this.drag = this.drag.bind(this);
+        this.dragEnd = this.dragEnd.bind(this);
+        this.#init(target);
+    }
+
+    #init(target) {
+        target.addEventListener('mousedown', (e) => {
+            this.#dragstart(e);
+
+            const drag = (e) => {
+                this.#drag(e, this.#state);
+            };
+
+            const dragend = (e) => {
+                this.#dragend(e, this.#state);
+
+                document.removeEventListener('mousemove', drag);
+                document.removeEventListener('mouseup', dragend);
+            };
+
+            document.addEventListener('mousemove', drag);
+            document.addEventListener('mouseup', dragend);
+        });
+    }
+
+    #dragstart = noop;
+    #drag = noop;
+    #dragend = noop;
+
+    #state = {};
+
+    /**
+     * @param {any} state
+     */
+    set state(state) {
+        if (typeof state !== 'object' && state !== null) {
+            state = { value: state };
+        }
+        this.#state = Object.assign(this.#state, state || {});
+    }
+
+    dragstart(cb) {
+        this.#dragstart = cb;
+        return this;
+    }
+
+    drag(cb) {
+        this.#drag = cb;
+        return this;
+    }
+
+    dragEnd(cb) {
+        this.#dragend = cb;
+        return this;
+    }
+}
+
+const drag = (target) => {
+    return new Drag(target);
+};
+
+export { createElement, Component, drag };
